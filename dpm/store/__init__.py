edad9221986f4f3ee3f34ad387f09c21b720d363
Andreas Gocht-Zech <andreas.gocht-zech@sva.de>
2025-11-27 16:32:52 +0100

    add an reinstall option

--- dpm/store/__init__.py
+++ dpm/store/__init__.py
@@ -60,9 +60,7 @@ class Store:
                 print(f"{r.name} +{r.required_variants} -{r.forbidden_variants}")
 
     def get_package_mod(self, pkg):
-        logger.debug(
-            f"Loading package module for {pkg.pkg} from {self.repo / pkg.pkg}"
-        )
+        logger.debug(f"Loading package module for {pkg.pkg} from {self.repo / pkg.pkg}")
         spec = importlib.util.spec_from_file_location(
             "dpm.repo." + pkg.pkg, self.repo / pkg.pkg / "__init__.py"
         )
