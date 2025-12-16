9e8214107746d84e6f14a48ae004289adf62e245
Christian von Elm <christian.von_elm@tu-dresden.de>
2025-12-16 10:31:59 +0100

    add-pkg(catch2)

---
+++ dpm/repo/catch2/__init__.py
@@ -0,0 +1,37 @@
+from dpm.downloader import WebResource
+from dpm.pkg_definition import Aspect, BasePackageRecipe
+
+from dpm.types import Needs
+
+
+class PackageRecipe(BasePackageRecipe):
+    def __init__(self, store, name):
+        super().__init__(store, name)
+
+    def needs(self) -> list[Needs]:
+        return [Needs("cc"), Needs("base"), Needs("cmake")]
+
+    def aspects(self) -> list[Aspect]:
+        return [Aspect.CONTAINS_BINARIES, Aspect.CONTAINS_PKG_CONFIG]
+
+    def sources(self):
+        return [
+            WebResource(
+                self,
+                "https://github.com/catchorg/Catch2/archive/refs/tags/v3.11.0.tar.gz",
+            ),
+        ]
+
+    def prepare(self):
+        pass
+
+    def create(self):
+        self.store.resolve(Needs("cmake")).cmake(
+            self,
+            "../Catch2-3.11.0/",
+            [],
+        )
+        self.make()
+
+    def install(self):
+        self.make("install")
